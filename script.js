0efa7920bf9545de4c1043ce6d17a4bb65b2bc08
EvgeniyR1987 <103533983+EvgeniyR1987@users.noreply.github.com>
2022-12-13 15:30:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function mainblock(){
       $('.mainblock').css('min-height',h);
 }
       mainblock();
+baguetteBox.run('.gallery', {
+        captions: true,
+        buttons: 'auto',
+        fullScreen: false,
+        noScrollbars: false,
+        titleTag: true,
+        async: false,
+        preload: 2,
+        animation: 'fadeIn',
+        afterShow: function() {
+            alert('Thanks');
+        },
+        afterHide: function() {
+            alert('Watch again');
+        },
+        onChange: null,
+        overlayBackgroundColor: 'rgba(113,117,115,0.8)'
+    });
