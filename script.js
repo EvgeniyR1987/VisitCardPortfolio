2e1a868a51da4da362b8b207ef73ba1daf1fe7eb
EvgeniyR1987 <103533983+EvgeniyR1987@users.noreply.github.com>
2022-12-13 11:40:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ function mainblock(){
       $('.mainblock').css('min-height',h);
 }
       mainblock();
+
+baguetteBox.run('.gallery');
+window.addEventListener('load', function() {
+  baguetteBox.run('.gallery');
+});
