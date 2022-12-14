d359973db0b64ec153b70fc33075b2624089a5ab
EvgeniyR1987 <103533983+EvgeniyR1987@users.noreply.github.com>
2022-12-14 17:44:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function mainblock(){
 
 const gallery = baguetteBox.run('.gallery', {
 });
+
+$('.filter__item').click(function(event) {
+  var i=$(this).data('filter');
+  if (i==1) {
+    $('.portfolio__column').show();
+  }else{
+    $('.portfolio__column').hide();
+    $('.portfolio__column.f_'+i).show();
+  }
+  $('.filter__item').removeClass('active');
+  $(this).addClass('active');
+  return false;
+});
